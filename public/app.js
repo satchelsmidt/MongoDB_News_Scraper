63f00a68af156e6ef051367bc1896469077c8ec1
supersatchel <34499356+satchelsmidt@users.noreply.github.com>
2019-11-03 21:34:29 -0800

    styled, completed DB connection with articles, ability to save multiple notes to article

--- public/app.js
+++ public/app.js
@@ -1,38 +1,111 @@
-$("#scrapeButton").on("click", function (e) {
-    e.preventDefault()
-
+$(document).ready(
     $.ajax({
         method: "GET",
         url: "/scrape"
     }).then(function () {
-        console.log('congratulations, you got to the second part of your API call')
-        $.ajax({
-            method: "GET",
-            url: "/articles"
-        }).then(function (data) {
-            console.log('congratulations, you maybe finished your entire api call')
-            console.log("DATA?: ", data)
+        // console.log('congratulations, you got to the second part of your API call')
+    })
+)
 
-            // var articleList = $("#articleSection")
+$("#scrapeButton").on("click", function (e) {
+    window.location.reload();
 
-            // var renderArticles = Handlebars.compile(articleList)
-
-            // articleList.html(renderArticles({
-            //     articles: data
-            // })) 
-
-            // for (let i = 0; i < data.length; i++) {
-            //     var article = {
-            //         title: data[i].title,
-            //         author: data[i].author,
-            //         link: data[i].link,
-            //         image: data[i].img,
-            //         blurb: data[i].blurb
-            //     }
-
-            //     console.log("ARTICLE OR SOMETHING: ", article)
-            // }
-
-        })
+    $.ajax({
+        method: "GET",
+        url: "/"
+    }).then(function (data) {
+        // console.log("DATA IS RIGHT HERE: ", data)
     })
 });
+
+$(document).on("click", ".noteButton", function (event) {
+    $("#noteDiv").empty();
+    var thisId = $(this).attr("data-id");
+    var thisTitle = $(this).attr("data-title");
+
+    $("#noteDiv").css('display', 'block');
+
+    console.log("ID OF BUTTON U CLICK:", thisId)
+
+    console.log("This is the 'this' of your button click:", $(this))
+
+    $.ajax({
+        method: "GET",
+        url: "/articles/" + thisId
+    })
+        .then(function (data) {
+            console.log("DATA THAT IS RETURNED ON ARTICLE THING: ", data);
+
+            var noteContent = $("<div>")
+            noteContent.attr("class", 'noteContent')
+
+            noteContent.append(thisTitle);
+            noteContent.append("<br>");
+            noteContent.append("<br>");
+            noteContent.append("<input id='titleinput' name='title' placeholder='Note Title'>");
+            noteContent.append("<br>")
+            noteContent.append("<br>")
+            noteContent.append("<textarea id='bodyinput' name='body' placeholder='Note Content'></textarea>");
+            noteContent.append("<br>")
+            noteContent.append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+
+            noteContent.append("<span class='close'>&times;</span>");
+
+            noteContent.append("<br>")
+            noteContent.append("<br>")
+           
+
+            $("#noteDiv").append(noteContent)
+
+            console.log("data note???data: ", data.note)
+
+            if (data.note) {
+
+                noteContent.append("<h2><strong>Article Notes:</strong> </h2>")
+                noteContent.append("<hr>");
+
+    
+                for (let i = 0; i < data.note.length; i++) {
+                    
+                    noteContent.append("<p><em>Title:</em> " + data.note[i].title + "</p>");
+                    noteContent.append("<p><em>Body:</em> " + data.note[i].body + "</p>");
+                    noteContent.append("<hr>");
+                }
+                
+            }
+        })
+})
+
+$(document).on("click", ".close", function(){
+
+$("#noteDiv").css('display', 'none');
+
+});
+
+// window.onclick = function(event) {
+//     if (event.target == $("#noteDiv")) {
+//         $("#noteDiv").css('display', 'none');
+//     }
+//   }
+
+$(document).on("click", "#savenote", function() {
+    var thisId = $(this).attr("data-id");
+  
+    $.ajax({
+      method: "POST",
+      url: "/articles/" + thisId,
+      data: {
+        title: $("#titleinput").val(),
+        body: $("#bodyinput").val()
+      }
+    })
+    .then(function(data) {
+      console.log(data);
+      $("#notes").empty();
+    });
+  
+    $("#titleinput").val("");
+    $("#bodyinput").val("");
+  });
+
+
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 //import express for server setup
 var express = require('express')
-
 var path = require("path")
 
 //import mongoogse
@@ -32,10 +31,12 @@ mongoose.connect("mongodb://localhost/newsScraperDB", { useNewUrlParser: true })
 const dbConnection = mongoose.connection;
 
 //listen for db connection error
+dbConnection.on("error", console.error.bind(console, "connection error:"))
+
+//look for db connection success
 dbConnection.once('open', () => {
     console.log('connected to mongoose!')
 });
-
 
 //import express handlebars for use
 var exphbs = require('express-handlebars');
@@ -45,136 +46,130 @@ app.engine("handlebars", exphbs({
 }));
 app.set("view engine", "handlebars")
 
+//this is home route: hits DB and grabs/displays all articles within
 app.get("/", function (req, res) {
-    res.render("index")
+    // console.log("COUNT OF ITEMS IN DB:", dbConnection.connection('Article').count)
+    db.Article.find({})
+        .then(function (dbArticle) {
+
+            var hbsObject = {
+                articles: dbArticle
+            }
+            console.log('this is your handlebar ', hbsObject)
+
+            res.render("index", hbsObject)
+        })
+        .catch(function (err) {
+            res.json(err)
+        })
 })
 
 //route that collects data from website
 app.get("/scrape", function (req, res) {
 
+    //Clear out database before we scrape more data (do not use this)
+    // db.Article.deleteMany({})
+    //     .then(function () {
+    //         console.log("Database Cleared")
+    //     })
+
+    //axios call to scrape data from website and put into object format
     axios.get("https://www.theatlantic.com/technology/").then(function (response) {
 
         var $ = cheerio.load(response.data);
 
         var result = {};
 
-        // var count = 10 
-
-        // console.log('YOUR COUNT AT THE BEGINNING, fool', count)
-
-        // if(count >= 0){
-
         $(".c-most-popular__item").each(function (i, element) {
 
-            result.img = $(element).find(".c-most-popular__img").attr("data-src");
-
-            if (result.img === undefined) {
-                return true
-            };
-
-            console.log("IMAGE:", result.img);
-
-            result.link = $(element).find(".c-most-popular__hed-link").attr("href");
-
-            if (result.link === undefined) {
-                return true
-            };
-
-            console.log("LINK:", result.link);
-
             result.title = $(element).find(".c-most-popular__hed-link").text().trim();
-
             if (result.title === undefined) {
                 return true
             };
 
-            console.log("TITLE:", result.title);
+            result.img = $(element).find(".c-most-popular__img").attr("data-src");
+            if (result.img === undefined) {
+                return true
+            };
+
+            result.link = $(element).find(".c-most-popular__hed-link").attr("href");
+            if (result.link === undefined) {
+                return true
+            };
 
             result.blurb = $(element).find(".c-most-popular__dek").text().trim();
-
             if (result.blurb === undefined) {
                 return true
             };
 
-            console.log("BLURB:", result.blurb);
-
             result.author = $(element).find(".c-most-popular__byline-author-link").text();
-
             if (result.author === undefined) {
                 return true
             };
 
-            console.log("AUTHOR:", result.author);
+            //Save articles (to be added at later date)
+            // result.isSaved = false
 
-            result.isSaved = false
+            //Attempt to parse existing db records out, failed
+            // db.Article.countDocuments({ title: result.title }, function (err, count) {
+            //     console.log("COUNT OF DOCS WITH SAME TITLE: ", count)
+            //     if (count > 1) {
+            //         return true
+            //     }
+            // })
 
+            //Create article field in DB
             db.Article.create(result)
                 .then(function (dbArticle) {
+                    // console.log("THIS IS A DB ARTICLE: ", dbArticle)
 
-                    return 
-                    console.log("THIS IS A DB ARTICLE: ", dbArticle)
+                    return
                 })
                 .catch(function (err) {
                     console.log(err)
                 });
-        
-        // count = count - 1
-
         });
-
-    // }
-
+        //Alert user when scrape is completed
+        console.log("SCRAPE IS FINISHED!")
         res.send("Scrape Completed")
     });
 });
 
-//this route returns ALL scraped articles
+//route to return one article
+app.get("/articles/:id", function (req, res) {
+    db.Article.findOne({ _id: req.params.id })
+        //this line puts in all data from related note object
+        .populate("note")
+        .then(function (dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            res.json(err)
+        })
+})
+
+//route to return all articles
 app.get("/articles", function (req, res) {
     db.Article.find({})
+        //this line puts in all data from related note object
         .then(function (dbArticle) {
-            console.log("BLAH BLAH BLAH DB ARTICLE OR SOMETHING BLAH: ", dbArticle)
-
-            // var hbsObject = {
-            //     articles: dbArticle
-            // }
-
-            // console.log('this is your handlebar ', hbsObject)
-
-            // res.render("index", hbsObject)
+            res.json(dbArticle);
         })
-        // .catch(function (err) {
-        //     res.json(err)
-        // })
+        .catch(function (err) {
+            res.json(err)
+        })
 })
 
 //create note route
-app.post("/articles/:id", function(req, res) {
+app.post("/articles/:id", function (req, res) {
     db.Note.create(req.body)
-      .then(function(dbNote) {
-        //fulfill the association between then note and the article
-        //find the article that is referenced in id
-        //reassign note to be the note we just created
-        //new: true option returns the updated article. If we do not include this, our query will not return the updated document
-        return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
-      })
-      .then(function(dbArticle) {
-        res.json(dbArticle);
-      })
-      .catch(function(err) {
-        res.json(err);
-      });
-  });
-  
-
-
-//this route returns SAVED articles
-app.get("/articles/saved", function (req, res) {
-    db.Article.find({
-        isSaved: true
-    })
-        //similar to include: [db.Note] that we would see in sequelize
-        //fills in 'note' field with data from the note object fields (rather than jsut a note id)
-        .populate("note")
+        .then(function (dbNote) {
+            //fulfill the association between then note and the article
+            //find the article that is referenced in id
+            //reassign note to be the note we just created
+            //new: true option returns the updated article. If we do not include this, our query will not return the updated document
+            return db.Article.findOneAndUpdate({ _id: req.params.id }, {$push: { note: dbNote._id }}, { new: true });
+        })
         .then(function (dbArticle) {
             res.json(dbArticle);
         })
@@ -183,20 +178,21 @@ app.get("/articles/saved", function (req, res) {
         });
 });
 
-//This route creates a new note 
-//   app.post()("/articles/:id", function(req, res){
-//       db.Note.create(req.body)
-//       .then(function(dbNote){
-//           return db.Article.findOneAndUpdate
-//           ({ _id: req.params.id}, push({note: dbNote._id}), { new:true});
-//       })
-//       .then(function(dbArticle){
-//           res.json(dbArticle);
-//       })
-//       .catch(function(err){
-//           res.json(err)
-//       })
-//   })
+// //this route returns SAVED articles (feature not yet implemented)
+// app.get("/articles/saved", function (req, res) {
+//     db.Article.find({
+//         isSaved: true
+//     })
+//         //similar to include: [db.Note] that we would see in sequelize
+//         //fills in 'note' field with data from the note object fields (rather than jsut a note id)
+//         .populate("note")
+//         .then(function (dbArticle) {
+//             res.json(dbArticle);
+//         })
+//         .catch(function (err) {
+//             res.json(err);
+//         });
+// });
 
 app.listen(PORT, function () {
     console.log("App running at http://localhost:" + PORT)
